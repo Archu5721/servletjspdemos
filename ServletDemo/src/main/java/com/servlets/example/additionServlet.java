798c71c8dd2022bb7dd53c996c0a723d1668892d
Archatha <archatha18022@cse.ssn.edu.in>
2022-08-01 13:22:13 +0530

    changes

--- ServletDemo/src/main/java/com/servlets/example/additionServlet.java
+++ ServletDemo/src/main/java/com/servlets/example/additionServlet.java
@@ -8,21 +8,18 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class HelloWorldServlet extends GenericServlet{
+public class additionServlet extends GenericServlet{
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		res.setContentType("text/html");
-		PrintWriter pw = res.getWriter();
-		pw.println("<html>");
-		pw.println("<body>");
-		pw.println("<h1>Hello World!!</h1>");
-		pw.println("</body>");
-		pw.println("</html>");
+		int number1 = Integer.parseInt(req.getParameter("number1"));
+		int number2 = Integer.parseInt(req.getParameter("number2"));
 		
+		int result = number1 + number2;
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<h1> Result is : " + result + "</h3>");
 	}
-
-	
 
 }
