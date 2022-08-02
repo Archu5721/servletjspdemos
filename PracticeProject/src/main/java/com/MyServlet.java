26ba29532ae2ebea2adc9176ae847e37d113fdaa
Archatha <archatha18022@cse.ssn.edu.in>
2022-08-02 12:11:12 +0530

    jdbc

---
+++ PracticeProject/src/main/java/com/MyServlet.java
@@ -0,0 +1,61 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    	
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		PrintWriter out = response.getWriter();
+//		out.println("Inside Service Method");
+//		
+//	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("Inside doGet Method");
+		out.println("Hello " + name);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("Inside doPost Method");
+		out.println("Hello " + name);
+		
+	}
+
+}
---
+++ jdbcbasics/src/jdbcbasics/jdbcTest.java
@@ -0,0 +1,39 @@
+package jdbcbasics;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class jdbcTest {
+	public static void main(String[] args) {
+		//readDb();
+		updateDb();
+		
+	}
+	
+	static void updateDb() {
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "Archu@2000");
+				Statement statement = connection.createStatement();) {
+			String qry = "update account set balance = 30000 where accno=3";
+			statement.executeUpdate(qry);
+			System.out.println("Successfully updated");
+		} catch (SQLException e) {
+		e.printStackTrace();
+		}
+	}
+	static void readDb(){
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "Archu@2000");
+				Statement statement = connection.createStatement();) {
+		
+			ResultSet rs = statement.executeQuery("select * from account");
+			while(rs.next()) {
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
+			}
+		} catch (SQLException e) {
+		e.printStackTrace();
+		}
+	
+	}
+}
